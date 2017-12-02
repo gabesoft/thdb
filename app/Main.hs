f29acbaaf9f863f119ba1c580b010914be573061
Gabriel Adomnicai <gabesoft@gmail.com>
2017-12-01 23:21:21 -0800

    Main functionality complete

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import Control.Monad (void)
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State
+import Data.Map.Strict as M
 import Engine
 import System.Environment (getArgs)
 import System.IO
@@ -7,14 +12,19 @@ import System.IO
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
-  repl stdin
+  void $ runStateT (repl stdin) Db { values = M.empty, stack = [] }
 
-repl :: Handle -> IO ()
+repl :: Handle -> StateT Db IO ()
 repl stdin = do
-  cmd <- readCmd stdin
+  cmd <- liftIO $ readCmd stdin
   case cmd of
     END -> return ()
-    _ -> run cmd >> repl stdin
+    _ -> do
+      out <- exec cmd
+      case out of
+        Just output -> liftIO $ putStrLn output
+        Nothing -> return ()
+      repl stdin
 
 readCmd :: Handle -> IO Command
 readCmd handle = read <$> hGetLine handle
--- src/Engine.hs
+++ src/Engine.hs
@@ -2,6 +2,10 @@
 -- Database engine
 module Engine where
 
+import Control.Monad.Trans.State
+import Data.Map.Strict as M
+import Prelude as P
+
 data Command =
     SET String String
   | GET String
@@ -13,3 +17,55 @@ data Command =
   | END
   deriving (Eq, Read, Show)
 
+data Db = Db {
+      values:: Map String String,
+      stack:: [ Map String String ]
+    }
+
+-- ^
+-- Execute a database command
+exec :: Monad m => Command -> StateT Db m (Maybe String)
+exec (GET name) = do
+  values <- gets values
+  return $ case M.lookup name values of
+             Just x -> Just x
+             Nothing -> Just "NULL"
+exec (SET name value) = do
+  db <- get
+  put $ db { values = M.insert name value $ values db }
+  return Nothing
+exec (UNSET name) = do
+  db <- get
+  put $ db { values = M.delete name $ values db}
+  return Nothing
+exec (NUMEQUALTO name) = do
+  values <- gets values
+  return $ Just $ show (count name values)
+exec BEGIN = do
+  db <- get
+  put $ db { stack = values db : stack db }
+  return Nothing
+exec ROLLBACK = do
+  db <- get
+  let st = stack db
+  if P.null st
+    then return (Just "NO TRANSACTION")
+    else do
+        put Db { stack = tail st, values = head st }
+        return Nothing
+exec COMMIT = do
+  db <- get
+  let st = stack db
+  if P.null st
+     then return (Just "NO TRANSACTION")
+     else do
+       put $ db { stack = [] }
+       return Nothing
+
+-- ^
+-- Count the entries with the given value
+count :: String -> Map String String -> Integer
+count value = M.foldr go 0
+  where go v c
+           | v == value = c + 1
+           | otherwise = c
